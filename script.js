00a0fe6012a8eded412db8c3c503a1cf9a6c2c2a
skybru <giorgio.bruschi98@gmail.com>
2024-10-17 12:58:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const pokeInput = document.getElementById("search-input");
+const pokeForm = document.getElementById("search-container");
+const pokeName = document.getElementById("pokemon-name");
+const pokeId = document.getElementById("pokemon-id");
+const pokeWeight = document.getElementById("weight");
+const pokeHeight = document.getElementById("height");
+const pokeTypes = document.getElementById("types");
+const pokeHp = document.getElementById("hp");
+const pokeAttack = document.getElementById("attack");
+const pokeDefense = document.getElementById("defense");
+const pokeSpAtt = document.getElementById("special-attack");
+const pokeSpDef = document.getElementById("special-defense");
+const pokeSpeed = document.getElementById("speed");
+const pokeSprite = document.getElementById("sprite");
+const pokeEndpoint = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
+
+const fetchData = async (input) => {
+    let allData = {};
+
+    try {
+        const allPokemon = await fetch(pokeEndpoint);
+        allData = await allPokemon.json();
+        //console.log(allData);
+    } catch (e) {
+        console.log(e);
+    }
+
+    const {results} = allData;
+
+    for (item of results) {
+        const {id, name, url} = item;
+        //number, string, string
+        //console.log(typeof(input)) = string
+
+        if (id.toString() === input || name === input ) {
+            //console.log(url);
+            return showPokemonData(url);
+        }
+    }
+    
+    alert("Pokémon not found");
+};
+
+const showPokemonData = async (pokeUrl) => {
+    let pokeDataJson = {};
+
+    try {
+        const pokemonData = await fetch(pokeUrl);
+        pokeDataJson = await pokemonData.json();
+        //console.log(pokeDataJson);
+    } catch (e) {
+        console.log(e);
+    }
+
+    const {name, id, weight, height, stats, sprites, types} = pokeDataJson;
+
+    pokeName.innerHTML += `${name.toUpperCase()}`;
+    pokeId.innerHTML += `#${id}`;
+    pokeWeight.innerHTML += `${weight}`;
+    pokeHeight.innerHTML += `${height}`;
+    pokeTypes.innerHTML = types.map(item => {
+        return `<div id=${item.type.name}>${item.type.name.toUpperCase()}</div>`;
+    }).join("");
+    pokeSprite.innerHTML = `<img src=${sprites.front_default}>`;
+    pokeHp.innerHTML += stats[0].base_stat;
+    pokeAttack.innerHTML += stats[1].base_stat;
+    pokeDefense.innerHTML += stats[2].base_stat;
+    pokeSpAtt.innerHTML += stats[3].base_stat;
+    pokeSpDef.innerHTML += stats[4].base_stat;
+    pokeSpeed.innerHTML += stats[5].base_stat;
+};
+
+const formatInput = (input) => {
+    const regex = /(\s)|[^a-zA-Z0-9-]/g;
+    const formatted = input.toLowerCase().replace(regex, "");
+    //console.log(formatted);
+    fetchData(formatted);
+};
+
+pokeForm.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevent page refresh after clicking submit
+    formatInput(pokeInput.value);
+    pokeName.innerHTML = "Name: ";
+    pokeId.innerHTML = "ID: ";
+    pokeWeight.innerHTML = "Weight: ";
+    pokeHeight.innerHTML = "Height: ";
+    pokeHp.innerHTML = "HP: "
+    pokeAttack.innerHTML = "Att: ";
+    pokeDefense.innerHTML = "Def: ";
+    pokeSpAtt.innerHTML = "Sp. Att: ";
+    pokeSpDef.innerHTML = "Sp. Def: ";
+    pokeSpeed.innerHTML = "Speed: ";
+});
+
+
+
+
+
